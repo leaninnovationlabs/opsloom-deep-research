b77f21d450001a1a335b709086c44175ec441cc7
ajfiala <andrewfiala4@gmail.com>
2025-03-14 11:32:33 -0400

    removed examples and tests

--- examples/hotel_app/backend/schemas/session.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from uuid import UUID
-
-class SessionSchema(BaseModel):
-    session_id: str = None
-    guest_id: str
-
-    class Config:
-        from_attributes = True
